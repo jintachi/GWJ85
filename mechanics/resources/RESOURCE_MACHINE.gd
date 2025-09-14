768b286b01a7f717107d60238238ab07a06fcdf8
Boris Wawrik <bwawrik@gmail.com>
2025-09-14 09:00:55 -0400

    machine resrouce update

--- mechanics/resources/RESOURCE_MACHINE.gd
+++ mechanics/resources/RESOURCE_MACHINE.gd
@@ -4,8 +4,11 @@ class_name Machine
 
 #enum machine_name {PRODUCER, TRANSPORTER, PROCESSOR, DELIVERER} 
 @export var machine_type: Genum.TileType
-@export var texture: AtlasTexture
-@export var level: int
+@export var texture: AtlasTexture  ## what it looks like
+@export var level: int  ## level it up
+@export var id : int ## The ID of the tile.
+@export var produced_item : GameItem ## What the Producer should be making.
+@export var production_period : int = 10 ## A measure of how many seconds it takes to produce 1 item
 
 func Changed():
 	emit_changed()
