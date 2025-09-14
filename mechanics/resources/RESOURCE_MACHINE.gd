8a5344c9123aaa53ea846e901b6de32ad2a3f91f
Boris Wawrik <bwawrik@gmail.com>
2025-09-14 08:58:28 -0400

    d

--- mechanics/resources/RESOURCE_MACHINE.gd
+++ mechanics/resources/RESOURCE_MACHINE.gd
@@ -2,9 +2,8 @@ extends Resource
 
 class_name Machine
 
-enum machine_name {PRODUCER, TRANSPORTER, PROCESSOR, DELIVERER} 
-
-@export var type: machine_name
+#enum machine_name {PRODUCER, TRANSPORTER, PROCESSOR, DELIVERER} 
+@export var machine_type: Genum.TileType
 @export var texture: AtlasTexture
 @export var level: int
 
