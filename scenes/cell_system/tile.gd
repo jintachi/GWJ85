3153a60b8a9c986ff44f5aecdbc7c1b715150047
ModeRogueGames <moderoguegames@gmail.com>
2025-09-15 15:18:56 -0400

    cleaning up previous select logic

--- scenes/cell_system/tile.gd
+++ scenes/cell_system/tile.gd
@@ -23,9 +23,7 @@ func _ready() -> void:
 	
 	set_themes()
 	self.add_theme_stylebox_override("panel",unselected_theme)
-	
-	# selection box when clicked
-	$Select.visible = selected
+
 	
 #endregion
 
@@ -46,7 +44,6 @@ func report_function() -> void:
 			print("Deliver")
 
 func toggle_selection(is_selected:bool) -> void :
-	selected = !selected 
 	#$Select.visible = selected
 	if selected:
 		parent.selected_tile = self
@@ -56,6 +53,7 @@ func toggle_selection(is_selected:bool) -> void :
 		self.remove_theme_stylebox_override("panel")
 		self.add_theme_stylebox_override("panel",unselected_theme)
 
+	selected = !selected 
 
 func set_themes() -> void:
 	selected_theme = theme.get_stylebox("selected","Tile")
@@ -69,17 +67,15 @@ func _on_gui_input(event: InputEvent) -> void:
 		return
 	
 	if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		#parent._UnselectAll()
-		toggle_selection(selected)
-		#selected = !selected 
-		#$Select.visible = selected
-		#if selected:
-			#parent.selected_tile = self
-		#else:
-		#	parent.selected_tile = null
+		if Input.is_physical_key_pressed(KEY_SHIFT) or Input.is_physical_key_pressed(KEY_CTRL):
+			toggle_selection(selected)
+		else :
+			parent._UnselectAll()
+			toggle_selection(selected)
+
 		
 func _unselect() -> void:
 	selected = false
-	$Select.visible = selected
+	toggle_selection(selected)
 	
 #endregion
