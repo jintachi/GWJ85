b213df483f38f5a3c8b9415aacf79653c553a428
Boris Wawrik <bwawrik@gmail.com>
2025-09-17 21:15:13 -0400

    recipes
    bread
    popcorn
    toasted pumkinseed
    corn chowder

--- mechanics/InventorySystem/CraftingRecipe.gd
+++ mechanics/InventorySystem/CraftingRecipe.gd
@@ -6,3 +6,12 @@ class_name CraftingRecipe
 @export var unlocked: bool = true
 @export var item: GameItem
 @export var ingredients: Array[InventorySlot]
+
+
+## check if ingredients are in inventory
+func _canMake() -> bool:
+	for i in ingredients:
+		if GameGlobal.myInventory.HasItem(i.item, i.amount) == false:
+			return false
+	return true
+	
