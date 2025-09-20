47b78d75b61ac7f9fc93e0207f2b118d9f0aa923
Rile-Cry <rileycriddle@yahoo.com>
2025-09-20 16:17:41 -0400

    Load game from main

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,9 @@
 extends Node
 
+@export var next_scene : PackedScene
+
 #region Built-Ins
 func _ready() -> void:
 	SceneManager.main_scene = self
+	SceneManager.to_next_scene(next_scene)
 #endregion
